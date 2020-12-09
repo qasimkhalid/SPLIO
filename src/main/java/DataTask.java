421d2731add1a3fdb703245119a0d1f8196613fc
Qasim Khalid <48443920+qasimkhalid@users.noreply.github.com>
2020-12-09 15:00:29 +0100

    Update DataTask.java

--- src/main/java/DataTask.java
+++ src/main/java/DataTask.java
@@ -2,16 +2,11 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
-
-
-
-
 
 public class DataTask{
 
